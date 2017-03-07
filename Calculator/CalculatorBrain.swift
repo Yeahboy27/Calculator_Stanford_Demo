cd4acd8f637811b74e2a39e8001369591a79c3d7
ThangTruong <thangtm6@fsoft.com.vn>
2017-03-07 17:50:03 +0700

    Have issue need to resolve

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -13,12 +13,23 @@ import Foundation
 class CalculatorBrain {
     
     private var accumulator = 0.0
+    var operation: Dictionary <String, Double> = [
+        "π": .pi,
+        "e": M_E,
+        "√": sqrt, // TODO: xuất hiện 1 vấn đề ở đây là operation của chúng ta không chỉ là số, mà còn là các phép tính toán nữa. Bạn hãy thử nghĩ cách làm thế nào để có thể nhét 1 function vào dictionary này?
+        "cos": cos
+    ]
+    
     func setOperand(operand: Double) {
         accumulator = operand
     }
     
-    func performOperation(operation: String) {
-        switch operation {
+    func performOperation(symbol: String) {
+        // TODO: Quiz: Tại sao operation không có chứa 1 optional nào nhưng kết quả trả ra của Opration[symbol] lại là 1 optional
+        if let constant = operation[symbol] {
+            accumulator = constant
+        }
+        switch symbol {
         case "π":
             accumulator = .pi
         case "√":
