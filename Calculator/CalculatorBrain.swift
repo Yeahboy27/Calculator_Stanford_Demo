3ad8df0f0fac242f825c969fe953d20a30d7364b
ThangTruong <thangtm6@fsoft.com.vn>
2017-03-07 17:31:47 +0700

    Apply pi and squart in Modal

--- Calculator/CalculatorBrain.swift
+++ Calculator/CalculatorBrain.swift
@@ -11,17 +11,27 @@ import Foundation
 // Chú ý: Model chỉ chứa dữ liệu, không bao giờ chứa giao diện nên nhất định ko được import UIKit vào đây
 
 class CalculatorBrain {
+    
+    private var accumulator = 0.0
     func setOperand(operand: Double) {
-        
+        accumulator = operand
     }
     
     func performOperation(operation: String) {
-         
+        switch operation {
+        case "π":
+            accumulator = .pi
+        case "√":
+            accumulator = sqrt(accumulator)
+            
+        default:
+            break
+        }
     }
     
     var result: Double {
         get {
-            return 0.0
+            return accumulator
         }
     }
 }
